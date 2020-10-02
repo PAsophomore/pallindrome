706b34e17df22e59a2d993ca798ed9d661535803
SagarGi <sagargurung1001@gmail.com>
2020-10-02 08:41:09 +0545

    Added Palindrome Number

--- pallindrome.java
+++ pallindrome.java
@@ -5,25 +5,27 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		  
-  int r,sum=0,temp;    
-  int n=454;//It is the number variable to be checked for palindrome  
-  
-  temp=n;    
-  while(n>0){    
-   r=n%10;  //getting remainder  
-   sum=(sum*10)+r;    
-   n=n/10;    
-  }    
-  if(temp==sum)    
-   System.out.println("palindrome number ");    
-  else    
-   System.out.println("not palindrome");    
-  
-	}
+class Codechef {
+   public static void main(String[] args) throws java.lang.Exception {
+      // your code goes here
+      int x = 545;
+      int num = x;
+      if (x < 0) {
+         System.out.println("The no is not Palindrome!!");
+      }
+
+      int reversed = 0;
+      while (x > 0) {
+         reversed = (reversed * 10) + x % 10;
+         x = x / 10;
+      }
+
+      System.out.println(reversed);
+
+      if (reversed == num) {
+         System.out.println("The no is palindrome!!");
+      } else {
+         System.out.println("The no is not palindrome!1");
+      }
+   }
 }
